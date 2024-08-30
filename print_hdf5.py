c742a9a333b9647beb471a15d9f062722ae4a522
nathanpalamuttam <npalamuttam04@gmail.com>
2024-08-29 20:42:57 -0400

    print_hdf5

---
+++ print_hdf5.py
@@ -0,0 +1,17 @@
+import h5py
+
+# Open the .hdf5 file
+file = h5py.File('/home2/npp8/data/n1_run0_prediction.hdf5', 'r')
+
+# List all groups
+print("Keys: %s" % file.keys())
+
+# Access a group or dataset
+dataset = file['your_dataset_name']
+
+# If you want to read the data
+data = dataset[:]
+print(data)
+
+# Don't forget to close the file when you're done
+file.close()
