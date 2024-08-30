205faa626726eab026d2de5f70980ac4ff016e4b
nathanpalamuttam <npalamuttam04@gmail.com>
2024-08-30 14:50:49 -0400

    print_lines .csv.gz

--- print_hdf5.py
+++ print_hdf5.py
@@ -1,17 +1,38 @@
 import h5py
+from pathlib import Path
+import numpy as np
+import pandas as pd
+from scipy.stats import pearsonr
 
-# Open the .hdf5 file
-file = h5py.File('/home2/npp8/data/n1_run0_prediction.hdf5', 'r')
+# out_dir = Path("/home2/ayh8/predictions/lcl_subsample/") # wherever your predictions are stored
+# procap_fp = "/home2/ayh8/data/lcl/fixed_windows/concat_procap_0.csv.gz" # wherever your experimental data is stored
+import gzip
+import csv
 
-# List all groups
-print("Keys: %s" % file.keys())
+# Replace 'your_file.csv.gz' with the path to your .csv.gz file
+with gzip.open('/fs/cbsubscb17/storage/projects/CLIPNET/data/gse110638/fixed_windows/data_folds/procap/concat_procap_0.csv.gz', mode='rt') as file:
+    reader = csv.reader(file)
+    for i, line in enumerate(reader):
+        if i < 5:
+            print(line)
+        else:
+            break
 
-# Access a group or dataset
-dataset = file['quantity']
+# procap = pd.read_csv(procap_fp, index_col=0, header=None)
 
-# If you want to read the data
-data = dataset[:]
-print(data)
-
-# Don't forget to close the file when you're done
-file.close()
+# runs = range(5)
+# for r in runs:
+#     names = tuple(filter(None, Path(f"n1_run{r}/lines.txt").read_text().split("\n")))
+#     out_fp = out_dir.joinpath(f"n1_run{r}_fold_0_predictions.h5")
+#     with h5py.File(out_fp, "r") as hf:
+#         profile = hf["track"][:]
+#         quantity = hf["quantity"][:]
+#         prediction = (profile / np.sum(profile, axis=1)[:, None]) * quantity
+#     # filter rows by name
+#     mask = procap.index.str.startswith(names)
+#     procap_sub = np.array(procap)[mask][:, np.r_[250:750, 1250:1750]]
+#     prediction_sub = prediction[mask]
+#     # calculate performance metrics
+#     profile_pearson = pd.DataFrame(procap_sub).corrwith(pd.DataFrame(prediction_sub), axis=1)
+#     quantity_pcc = pearsonr(np.log(procap_sub.sum(axis=1) + 1e-3), np.log(prediction_sub.sum(axis=1) + 1e-3))[0]
+#     print(f"Run {r}: {profile_pearson.median()}, {quantity_pcc}")
