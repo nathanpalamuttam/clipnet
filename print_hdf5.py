2ed56dc0aad4957f446783f3bf70d15409d3e89a
nathanpalamuttam <npalamuttam04@gmail.com>
2024-08-30 16:04:34 -0400

    code given

--- print_hdf5.py
+++ print_hdf5.py
@@ -3,28 +3,20 @@ from pathlib import Path
 import numpy as np
 import pandas as pd
 from scipy.stats import pearsonr
-import gzip
-import csv
 import numpy as np
-
-# Load the NPZ file
-
 import pandas as pd
 
 # Load the .csv.gz file
-data = pd.read_csv('/fs/cbsubscb17/storage/projects/CLIPNET/data/gse110638/fixed_windows/data_folds/procap/concat_procap_0.csv.gz')
+# data = pd.read_csv('/fs/cbsubscb17/storage/projects/CLIPNET/data/gse110638/fixed_windows/data_folds/procap/concat_procap_0.csv.gz')
 
-# Print the first cell of the first row
-first_cell = data.iloc[0, 0]
-print(first_cell)
-print()
-print(data.head())
+# # Print the first cell of the first row
+# first_cell = data.iloc[0, 0]
+# print(first_cell)
+# print()
+# print(data.head())
 
 # Get the length of the DataFrame (number of rows)
 #GSM3004658_51654_R1_cap.clip.CGATGT
-
-
-import h5py
 
 # Open the HDF5 file
 # with h5py.File('/home2/npp8/data/n1_run0_prediction.hdf5', 'r') as hf:
@@ -40,24 +32,24 @@ import h5py
 #             print(line)
 #         else:
 #             break
-# out_dir = Path("/home2/ayh8/predictions/lcl_subsample/") # wherever your predictions are stored
-# procap_fp = "/home2/ayh8/data/lcl/fixed_windows/concat_procap_0.csv.gz" # wherever your experimental data is stored
+out_dir = Path("/home2/npp8/data") # wherever your predictions are stored
+procap_fp = "/fs/cbsubscb17/storage/projects/CLIPNET/data/gse110638/fixed_windows/data_folds/procap/concat_procap_0.csv.gz" # wherever your experimental data is stored
 
-# procap = pd.read_csv(procap_fp, index_col=0, header=None)
+procap = pd.read_csv(procap_fp, index_col=0, header=None)
 
-# runs = range(5)
-# for r in runs:
-#     names = tuple(filter(None, Path(f"n1_run{r}/lines.txt").read_text().split("\n")))
-#     out_fp = out_dir.joinpath(f"n1_run{r}_fold_0_predictions.h5")
-#     with h5py.File(out_fp, "r") as hf:
-#         profile = hf["track"][:]
-#         quantity = hf["quantity"][:]
-#         prediction = (profile / np.sum(profile, axis=1)[:, None]) * quantity
-#     # filter rows by name
-#     mask = procap.index.str.startswith(names)
-#     procap_sub = np.array(procap)[mask][:, np.r_[250:750, 1250:1750]]
-#     prediction_sub = prediction[mask]
-#     # calculate performance metrics
-#     profile_pearson = pd.DataFrame(procap_sub).corrwith(pd.DataFrame(prediction_sub), axis=1)
-#     quantity_pcc = pearsonr(np.log(procap_sub.sum(axis=1) + 1e-3), np.log(prediction_sub.sum(axis=1) + 1e-3))[0]
-#     print(f"Run {r}: {profile_pearson.median()}, {quantity_pcc}")
+runs = range(5)
+for r in runs:
+    names = tuple(filter(None, Path(f"n1_run{r}/lines.txt").read_text().split("\n")))
+    out_fp = out_dir.joinpath(f"n1_run{r}_predictions.h5")
+    with h5py.File(out_fp, "r") as hf:
+        profile = hf["track"][:]
+        quantity = hf["quantity"][:]
+        prediction = (profile / np.sum(profile, axis=1)[:, None]) * quantity
+    # filter rows by name
+    mask = procap.index.str.startswith(names)
+    procap_sub = np.array(procap)[mask][:, np.r_[250:750, 1250:1750]]
+    prediction_sub = prediction[mask]
+    # calculate performance metrics
+    profile_pearson = pd.DataFrame(procap_sub).corrwith(pd.DataFrame(prediction_sub), axis=1)
+    quantity_pcc = pearsonr(np.log(procap_sub.sum(axis=1) + 1e-3), np.log(prediction_sub.sum(axis=1) + 1e-3))[0]
+    print(f"Run {r}: {profile_pearson.median()}, {quantity_pcc}")
