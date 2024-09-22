0c4cce7ef4bed83726a7c5c40ab9dfd8294d2811
nathanpalamuttam <npalamuttam04@gmail.com>
2024-09-22 17:57:47 -0400

    check intervals

--- view_bigwig.py
+++ view_bigwig.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import pickle
 import pyBigWig
 import gzip
 # Open the BigWig file
@@ -25,16 +26,28 @@ print(f"Header Details: {bw.header()}")
 
 # Display the first few intervals from the first chromosome
 print("\nFirst few intervals:")
-for key in bw.chroms():
-    chromosome = key
-    for elem in hashMapTSS[chromosome]:
-        try:
-            intervals = bw.intervals(chromosome, elem[0]-1, elem[1]-300)
-            for interval in intervals[:10]:
-                print(interval)
-        except Exception as e:
-            print(f"Error fetching intervals: {e}")
-        break
+for chromosome in bw.chroms():
+    if chromosome in hashMapTSS:
+        for elem in hashMapTSS[chromosome]:
+            try:
+                TSS = elem[0]
+                TES = elem[1]
+                intervals = bw.intervals(chromosome, TSS - 150, TES - 300)
+                if intervals:
+                    for interval in intervals:
+                        if interval[0] <= elem[0] + 150:
+                            elem[2] += 1
+                        else:
+                            elem[3] += 1
+            except Exception as e:
+                print(f"Error fetching intervals: {e}")
+output_file = "/home2/npp8/data/hashMapTSS.pkl"
+
+# Save hashMapTSS to the specified file using pickle
+with open(output_file, 'wb') as file:
+    pickle.dump(hashMapTSS, file)
+
+print(f"hashMapTSS has been successfully saved to {output_file}")
 # Replace 'chr1' with the appropriate chromosome name present in your BigWig file
 # chromosome = list(bw.chroms().keys())[0]  # Get the first chromosome in the file
 # try:
