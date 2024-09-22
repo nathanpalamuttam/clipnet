6dbcdbeeda74d04ce90cd60634724ef793a39c17
nathanpalamuttam <npalamuttam04@gmail.com>
2024-09-22 17:51:26 -0400

    test

--- view_bigwig.py
+++ view_bigwig.py
@@ -34,6 +34,7 @@ for key in bw.chroms():
                 print(interval)
         except Exception as e:
             print(f"Error fetching intervals: {e}")
+        break
 # Replace 'chr1' with the appropriate chromosome name present in your BigWig file
 # chromosome = list(bw.chroms().keys())[0]  # Get the first chromosome in the file
 # try:
