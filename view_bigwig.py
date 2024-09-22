1afdfde9348a682fcafdab6e5386e71cfcd37056
nathanpalamuttam <npalamuttam04@gmail.com>
2024-09-22 18:54:29 -0400

    strand

--- view_bigwig.py
+++ view_bigwig.py
@@ -8,16 +8,14 @@ line_count = 0
 hashMapTSS = defaultdict(list)
 #this is the TSS start and end sites
 with gzip.open("/fs/cbsubscb17/storage/projects/JIA_PROcap/JIA_PROcap_mapping/seq_merged/denr_greater_than_1rpb_tx.bed.gz", 'rt') as f:
-    for line in f:  # adjust the range as needed to see more lines
+    for line in f:
         temp = line.strip().split("\t")
         key = temp[0]
+        geneID = temp[3]
         TSS = int(temp[1])
         TES = int(temp[2])
         strand = temp[4]
-        if strand == '+':
-            hashMapTSS[key].append([TSS, TES, 0, 0])
-        else:
-            hashMapTSS[key].append([TES, TSS, 0, 0])
+        hashMapTSS[key].append([TSS, TES, 0, 0, strand, geneID])
 print(hashMapTSS.keys())
 
 #go through BigWig file and create hashmap where key is chromosome and value is a list
@@ -28,40 +26,36 @@ print("Header Information:")
 print(f"Chromosomes: {bw.chroms()}")
 print(f"Header Details: {bw.header()}")
 
-# Display the first few intervals from the first chromosome
-print("\nFirst few intervals:")
 for chromosome in bw.chroms():
     if chromosome in hashMapTSS:
         for elem in hashMapTSS[chromosome]:
             try:
                 TSS = elem[0]
                 TES = elem[1]
-                intervals = bw.intervals(chromosome, TSS - 150, TES - 300)
+                strand = elem[4]
+                if strand == '+':
+                    intervals = bw.intervals(chromosome, TSS - 150, TES - 300)
+                else:
+                    intervals = bw.intervals(chromosome, TSS + 300, TES + 150)
                 if intervals:
                     for interval in intervals:
-                        if interval[0] <= elem[0] + 150:
-                            elem[2] += 1
+                        if strand == '+':
+                            if interval[0] <= elem[0] + 150:
+                                elem[2] += 1
+                            else:
+                                elem[3] += 1
                         else:
-                            elem[3] += 1
+                            if interval[0] >= TES - 150:
+                                elem[2] += 1
+                            else:
+                                elem[3] += 1
             except Exception as e:
                 print(f"Error fetching intervals: {e}")
-output_file = "/home2/npp8/data/hashMapTSS.pkl"
 
-# Save hashMapTSS to the specified file using pickle
+
+output_file = "/home2/npp8/data/hashMapTSS.pkl"
 with open(output_file, 'wb') as file:
     pickle.dump(hashMapTSS, file)
 
 print(f"hashMapTSS has been successfully saved to {output_file}")
-# Replace 'chr1' with the appropriate chromosome name present in your BigWig file
-# chromosome = list(bw.chroms().keys())[0]  # Get the first chromosome in the file
-# try:
-#     intervals = bw.intervals(chromosome, 0, 1000)  # Fetch intervals within the first 1000 bases
-
-#     # Print the first 10 intervals
-#     for interval in intervals[:10]:  # Adjust the slice as needed
-#         print(interval)
-# except Exception as e:
-#     print(f"Error fetching intervals: {e}")
-
-# Close the BigWig file
 bw.close()
