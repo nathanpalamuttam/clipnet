213edded44325d165b76f37ec821d1d4f72bb277
nathanpalamuttam <npalamuttam04@gmail.com>
2024-10-04 23:01:31 -0400

    sadf

---
+++ pause_index/pause_ind.py
@@ -0,0 +1,134 @@
+"""
+This script calculates the pausing index for a set of genes. The pausing index is defined
+as the ratio of the average read density in the promoter region (150 bp upstream and
+downstream of the TSS) to the average read density in the gene body (300 bp downstream of
+the TSS to 300 bp upstream of the TES). The script takes as input a bed file of gene
+coordinates and two bigwig files of read densities (one for the plus strand and one for
+the minus strand). The script outputs a bed file with the pausing index for each gene.
+"""
+
+import argparse
+
+import numpy as np
+import pandas as pd
+import pyBigWig
+
+pd.options.mode.copy_on_write = True
+
+
+def pausing_index(
+    bw,
+    chrom,
+    start,
+    stop,
+    strand,
+    promoter_boundaries=(150, 150),
+    gene_body_boundaries=(300, 300),
+):
+    """
+    Calculate pausing index for a given region.
+    IMPORTANT: THIS ASSUMES THAT THE BW FILE IS THE CORRECT STRAND (pausing_index_from_bed
+    filters by strand, so this should not be an issue here, but if you use this function
+    elsewhere, please check that the supplied bw file is the correct one).
+    """
+    if strand == "+":
+        promoter_boundaries = [
+            start - promoter_boundaries[0],
+            start + promoter_boundaries[1],
+        ]
+        gene_body_boundaries = [
+            start + gene_body_boundaries[0],
+            stop - gene_body_boundaries[1],
+        ]
+    elif strand == "-":
+        promoter_boundaries = [
+            stop - promoter_boundaries[0],
+            stop + promoter_boundaries[1],
+        ]
+        gene_body_boundaries = [
+            start + gene_body_boundaries[0],
+            stop - gene_body_boundaries[1],
+        ]
+    else:
+        raise ValueError("Strand must be either '+' or '-'")
+    promoter_read_density = bw.stats(chrom, *promoter_boundaries, type="mean")[0]
+    gene_body_read_density = bw.stats(chrom, *gene_body_boundaries, type="mean")[0]
+    if promoter_read_density is None or gene_body_read_density is None:
+        return np.nan
+    return promoter_read_density / gene_body_read_density
+
+
+def pausing_index_from_bed(
+    pl_bw,
+    mn_bw,
+    bed,
+    min_length=2000,
+    promoter_boundaries=(150, 150),
+    gene_body_boundaries=(300, 300),
+):
+    """
+    Calculate pausing index for all regions in a bed file. (assumes bed file is a
+    pd.DataFrame with at least columns ["chrom", "start", "stop", "name", "strand"]
+    """
+    bed["gene_length"] = bed["stop"] - bed["start"]
+    clean_bed = bed[bed["gene_length"] > min_length]
+    pl_bed = clean_bed[clean_bed["strand"] == "+"]
+    mn_bed = clean_bed[clean_bed["strand"] == "-"]
+    pl_bed["pausing_index"] = pl_bed.apply(
+        lambda x: pausing_index(pl_bw, x["chrom"], x["start"], x["stop"], x["strand"]),
+        axis=1,
+    )
+    mn_bed["pausing_index"] = mn_bed.apply(
+        lambda x: pausing_index(mn_bw, x["chrom"], x["start"], x["stop"], x["strand"]),
+        axis=1,
+    )
+    out_bed = pd.concat([pl_bed, mn_bed])
+    out_bed["tss"] = np.where(
+        out_bed["strand"] == "+", out_bed["start"], out_bed["stop"]
+    )
+    out_bed["tss+1"] = out_bed["tss"] + 1
+    return out_bed[["chrom", "tss", "tss+1", "name", "pausing_index", "strand"]]
+
+
+def main(pl_bw_fp, mn_bw_fp, bed_fp, out_fp, **kwargs):
+    """
+    Wraps io functions and pausing_index_from_bed.
+    """
+    bed = pd.read_csv(bed_fp, sep="\t", header=None)
+    bed.columns = ["chrom", "start", "stop", "name", "strand", "score"]
+    pl_bw = pyBigWig.open(pl_bw_fp)
+    mn_bw = pyBigWig.open(mn_bw_fp)
+    out_bed = pausing_index_from_bed(pl_bw, mn_bw, bed, **kwargs)
+    out_bed.to_csv(out_fp, sep="\t", header=False, index=False)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Calculate pausing index for a set of genes."
+    )
+    parser.add_argument("pl_bw_fp", help="Path to plus strand bigwig file.")
+    parser.add_argument("mn_bw_fp", help="Path to minus strand bigwig file.")
+    parser.add_argument("bed_fp", help="Path to bed file.")
+    parser.add_argument("out_fp", help="Path to output bed file.")
+    parser.add_argument(
+        "--min_length",
+        type=int,
+        default=2000,
+        help="Minimum gene length to include in output.",
+    )
+    parser.add_argument(
+        "--promoter_boundaries",
+        type=int,
+        nargs=2,
+        default=(150, 150),
+        help="Number of bp upstream and downstream of TSS to include in promoter region.",
+    )
+    parser.add_argument(
+        "--gene_body_boundaries",
+        type=int,
+        nargs=2,
+        default=(300, 300),
+        help="Number of bp downstream of TSS and upstream of TES to trim off for the gene body.",
+    )
+    args = parser.parse_args()
+    main(args.pl_bw_fp, args.mn_bw_fp, args.bed_fp, args.out_fp)
---
+++ pause_index/sample_windows.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python3
+
+import argparse
+import random
+
+import pandas as pd
+import pybedtools
+from pybedtools.featurefuncs import midpoint
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Parse arguments
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+parser = argparse.ArgumentParser()
+parser.add_argument("bed", help="a bed file with genome coordinates")
+parser.add_argument(
+    "--out", help="where to write output (default is stdout)", default=None
+)
+parser.add_argument(
+    "--window", help="the size of the windows to get", type=int, default=1000
+)
+parser.add_argument(
+    "--buffer",
+    help="min space to window edge on both sides of peak (< window / 2)",
+    type=int,
+    default=250,
+)
+parser.add_argument(
+    "--reps", help="number of windows to take around each center", type=int, default=1
+)
+parser.add_argument("--seed", help="a seed for random", default=None)
+args = parser.parse_args()
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Perform window sampling
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+assert args.window > 0, "Window size needs to be a positive integer."
+assert (
+    0 < args.buffer < args.window / 2
+), "Buffer needs to be a positive integer less than window / 2."
+assert args.reps > 0, "reps needs to be a positive integer."
+
+# set side (for reproducibility)
+random.seed(args.seed)
+
+# import data
+df = pd.read_csv(args.bed, sep="\t", header=None)
+
+
+def sample_windows(peaks, window_length, pad, centered=False):
+    """Sample windows from self.bed_file"""
+    windows = []
+    for peak in peaks:
+        if centered:
+            window_init = midpoint(peak).start
+            window_start = window_init - int(window_length / 2)
+        else:
+            window_init = random.randrange(peak.start, peak.stop + 1)
+            window_start = int(window_init - random.randrange(pad, window_length - pad))
+        window_stop = window_start + window_length
+        windows.append([peak.chrom, window_start, window_stop])
+    return pybedtools.BedTool(windows)
+
+
+# iterate over all rows in df
+for i in range(df.shape[0]):
+    # perform args.reps samples
+    for rep in range(args.reps):
+        chrom, peak_start, peak_stop, name, score, strand = df.iloc[i][:3]
+        # pick a random place in the peak to build a window around
+        window_init = random.randrange(peak_start, peak_stop + 1)
+        # pick a random start point of the window
+        window_start = int(
+            window_init - random.randrange(args.buffer, args.window - args.buffer)
+        )
+        window_stop = int(window_start + args.window)
+        row = "\t".join(
+            [chrom, str(window_start), str(window_stop), name, str(score), strand]
+        )
+        if args.out is None:
+            print(row)
+        else:
+            # clear file
+            with open(args.out, "w+") as out:
+                out.write("")
+            with open(args.out, "a") as out:
+                out.write(row + "\n")
